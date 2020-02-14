d566f15eab7d94b228d3386b52be33fdc1c9b3e8
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-14 15:03:18 +0530

    Added Content Loss (VGG19)

--- src/losses.py
+++ src/losses.py
@@ -60,3 +60,41 @@ def PixelLoss(type='l1'):
         return tf.keras.losses.MeanAbsoluteError()
     else:
         return tf.keras.losses.MeanSquaredError()
+
+
+
+def ContentLoss(type='l1', output_layer=54, before_act=True):
+    '''Content Loss (VGG19)
+    Params:
+        type        -> 'l1'/'l2' norm
+        output_layer    -> Index of VGG19 output Layer
+        before_act      -> Include activation or not
+    '''
+    if type == 'l1':
+        loss_function = tf.keras.losses.MeanAbsoluteError()
+    else:
+        loss_function = tf.keras.losses.MeanSquaredError()
+    vgg = tf.keras.applications.VGG19(
+        input_shape=(None, None, 3),
+        include_top=False
+    )
+    if output_layer == 22:
+        pick_layer = 5
+    elif output_layer == 54:
+        pick_layer = 20
+    if before_act:
+        vgg.layers[pick_layer].activation = None
+    feature_extration_model = tf.keras.Model(
+        vgg.input,
+        vgg.layers[pick_layer].output
+    )
+
+    @tf.function
+    def content_loss(hr, sr):
+        preprocess_sr = tf.keras.applications.vgg19.preprocess_input(sr * 255.) / 12.75
+        preprocess_hr = tf.keras.applications.vgg19.preprocess_input(hr * 255.) / 12.75
+        sr_features = feature_extration_model(preprocess_sr)
+        hr_features = feature_extration_model(preprocess_hr)
+        return loss_func(hr_features, sr_features)
+    
+    return content_loss
