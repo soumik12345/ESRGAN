9f76d88be04cc0845cd7c3f0594cc9dd79f9b5fe
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-24 14:09:28 +0530

    Added Cosine Annealing LR Restart Strategy

--- src/learning_rate_schedulers.py
+++ src/learning_rate_schedulers.py
@@ -11,3 +11,10 @@ def MultiStepLR(initial_learning_rate, lr_steps, lr_rate, name='MultiStepLR'):
     )
 
 
+def CosineAnnealingLR_Restart(initial_learning_rate, t_period, lr_min):
+    '''Cosine annealing learning rate scheduler with restart'''
+    return tf.keras.experimental.CosineDecayRestarts(
+        initial_learning_rate=initial_learning_rate,
+        first_decay_steps=t_period, t_mul=1.0, m_mul=1.0,
+        alpha=lr_min / initial_learning_rate
+    )
