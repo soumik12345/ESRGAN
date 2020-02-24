a1f6ecce0b5c871ea6bbd99b46ccc1b20875fee4
Ubuntu <ubuntu@ip-172-31-45-210.ec2.internal>
2020-02-24 20:26:32 +0000

    Updated Pre-training Code

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from matplotlib import pyplot as plt
 
 
 def set_memory_growth():
@@ -13,3 +14,37 @@ def set_memory_growth():
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
+
+            
+            
+def visualize_batch(dataset, model=None):
+    x_batch, y_batch = next(iter(dataset))
+    x_batch = x_batch.numpy()
+    y_batch = y_batch.numpy()
+    c = 0
+    if model is None:
+        fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize = (16, 16))
+        plt.setp(axes.flat, xticks = [], yticks = [])
+        for i, ax in enumerate(axes.flat):
+            if i % 2 == 0:
+                ax.imshow(x_batch[c])
+                ax.set_xlabel('Low_Res_' + str(c + 1))
+            elif i % 2 == 1:
+                ax.imshow(y_batch[c])
+                ax.set_xlabel('High_Res_' + str(c + 1))
+                c += 1
+    else:
+        fig, axes = plt.subplots(nrows = 1, ncols = 3, figsize = (16, 16))
+        plt.setp(axes.flat, xticks = [], yticks = [])
+        for i, ax in enumerate(axes.flat):
+            if i % 3 == 0:
+                ax.imshow(x_batch[c])
+                ax.set_xlabel('Low_Res_' + str(c + 1))
+            elif i % 3 == 1:
+                ax.imshow(y_batch[c])
+                ax.set_xlabel('High_Res_' + str(c + 1))
+            elif i % 3 == 2:
+                ax.imshow(np.squeeze(SRTfrecordDataset.denormalize(model(np.expand_dims(x_batch[c], axis=0)))))
+                ax.set_xlabel('High_Res_' + str(c + 1))
+                c += 1
+    plt.show()
