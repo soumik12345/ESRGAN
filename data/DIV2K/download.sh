f85a4860f8acbe4e58229916128dc93162f78525
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-18 12:50:30 +0530

    Added Dataset Download Script

---
+++ data/DIV2K/download.sh
@@ -0,0 +1,6 @@
+!wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+!wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip
+!unzip -q DIV2K_train_HR.zip
+!unzip -q DIV2K_train_LR_bicubic_X4.zip
+!rm DIV2K_train_HR.zip
+!rm DIV2K_train_LR_bicubic_X4.zip
